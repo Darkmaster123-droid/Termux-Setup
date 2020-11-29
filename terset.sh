4c04baa5411652eb224960209b99e2e1b955c814
Darkmaster123-droid <68417769+Darkmaster123-droid@users.noreply.github.com>
2020-11-29 15:59:23 +0700

    Update terset.sh

--- terset.sh
+++ terset.sh
@@ -4,13 +4,13 @@ echo "# Scripter : Darkmaster123-droid"      #" | lolcat
 echo "# I hope this tools help you guys      #" | lolcat
 echo "########################################" | lolcat
 pkg update && pkg upgrade -y
-pkg install python
-pkg install python2
-pkg install php
-pkg install bin
-pkg install tmux
-pkg install figlet
-pkg install sah
-pkg install wireshark
+pkg install python -y
+pkg install python2 -y
+pkg install php -y
+pkg install cmatrix -y
+pkg install tmux -y
+pkg install figlet -y
+pkg install zsh -y
+pkg install wireshark -y
 echo "Done" | lolcat
 echo "Bye-Bye :)" | lolcat
