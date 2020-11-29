55e10824ee6e001934da941edd26e248570d438a
Darkmaster123-droid <68417769+Darkmaster123-droid@users.noreply.github.com>
2020-11-29 15:57:40 +0700

    Create terset.sh
    Srry if it so bad.cause i'm new at coding

---
+++ terset.sh
@@ -0,0 +1,16 @@
+clear
+echo "########################################" | lolcat
+echo "# Scripter : Darkmaster123-droid"      #" | lolcat
+echo "# I hope this tools help you guys      #" | lolcat
+echo "########################################" | lolcat
+pkg update && pkg upgrade -y
+pkg install python
+pkg install python2
+pkg install php
+pkg install bin
+pkg install tmux
+pkg install figlet
+pkg install sah
+pkg install wireshark
+echo "Done" | lolcat
+echo "Bye-Bye :)" | lolcat
